975dddf76b9b2c4af3af77a17ab62b7fa58ac696
Michał Kołacz <michal.kolacz45@gmail.com>
2025-05-02 10:53:27 +0200

    feat: integrate PostHog for analytics tracking and update dependencies

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,5 @@
-import { startTransition } from 'react'
+import posthog from 'posthog-js'
+import { startTransition, useEffect, StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import { HydratedRouter } from 'react-router/dom'
 
@@ -6,6 +7,24 @@ if (ENV.MODE === 'production' && ENV.SENTRY_DSN) {
 	void import('./utils/monitoring.client.tsx').then(({ init }) => init())
 }
 
+const PosthogInit = () => {
+	useEffect(() => {
+		posthog.init('phc_F1WbwxCyp5a6a8PKfe9caNENHcOaRnDWyXCCYeRkFDT', {
+			api_host: 'https://eu.i.posthog.com',
+			person_profiles: 'always',
+			// or 'always' to create profiles for anonymous users as well
+		})
+	}, [])
+
+	return null
+}
+
 startTransition(() => {
-	hydrateRoot(document, <HydratedRouter />)
+	hydrateRoot(
+		document,
+		<StrictMode>
+			<HydratedRouter />
+			<PosthogInit />
+		</StrictMode>,
+	)
 })
