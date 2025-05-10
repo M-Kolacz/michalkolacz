0b81b7c18349e3558abb88f78c40f734128492ca
Michał Kołacz <michal.kolacz45@gmail.com>
2025-05-10 10:58:17 +0200

    feat: make SENTRY_DSN required and use environment variable for POSTHOG_API_KEY

--- app/utils/feature-flags.server.ts
+++ app/utils/feature-flags.server.ts
@@ -2,7 +2,7 @@ import { remember } from '@epic-web/remember'
 import { PostHog } from 'posthog-node'
 
 export const posthogClient = remember('feature-flags-client', () => {
-	return new PostHog('phc_F1WbwxCyp5a6a8PKfe9caNENHcOaRnDWyXCCYeRkFDT', {
+	return new PostHog(process.env.POSTHOG_API_KEY, {
 		host: 'https://eu.i.posthog.com',
 	})
 })
