e11b3c4f69db570d53990ab0568e73b99cdeaed2
Michał Kołacz <michal.kolacz45@gmail.com>
2025-05-20 19:36:22 +0200

    feat: implement cache refresh logic via webhook

--- app/routes/action+/refresh-cache.tsx
+++ app/routes/action+/refresh-cache.tsx
@@ -1,10 +1,36 @@
+import { type PushEvent } from '@octokit/webhooks-types'
 import { data } from 'react-router'
+import { getBlogMdxListItems, getMdxPage } from '#app/utils/mdx.server.ts'
 import { type Route } from '../+types/me'
 
 export async function action({ request }: Route.ActionArgs) {
-	const body = await request.json()
+	// TODO: Add secret to verify the weebook sender
 
-	console.log('Refreshing cache', body)
+	const body = (await request.json()) as PushEvent
 
-	return data(body)
+	const isMaster = body.ref === 'refs/heads/master'
+
+	if (!isMaster) throw data('Webhook not from master branch', { status: 400 })
+
+	const changedFiles = new Set<string>()
+
+	body.commits.forEach((commit) => {
+		commit.modified.forEach((file) => changedFiles.add(file))
+	})
+
+	const filteredFiles = [...changedFiles].filter((file) => {
+		return file.startsWith('content/blog')
+	})
+
+	const keysToInvalidate = filteredFiles.map(
+		(file) => file.split('/')[2] as string,
+	)
+
+	for (const slug of keysToInvalidate) {
+		await getMdxPage({ contentDir: 'blog', slug }, { forceFresh: true })
+	}
+
+	await getBlogMdxListItems({ request, forceFresh: true })
+
+	return data({ filteredFiles, keysToInvalidate })
 }
